4939a503e2cdfa2476202c551fd839dd61b51a12
geetbrains <ylishnaf@yandex.ru>
2020-05-17 16:43:49 +0500

    hw

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
+        Move [] movables = {
+                new Human("Вася", 2, 5000),
+                new Cat("Мурзик", 3, 2000),
+                new Robot("C-3PO", 4,10000)
+        };
+
+        Obstacle [] obstacles = {
+                new RunningTrack(7000),
+                new RunningTrack(1000),
+                new Wall(1),
+                new Wall(3)
+        };
+
+        for (Move m: movables) {
+            for (Obstacle o: obstacles) {
+
+                o.doIt(m);
+                if (!m.isItCan()){
+                    break;
+                }
+                
+            }
+
+        }
 
     }
 }
--- src/Robot.java
+++ src/Robot.java
@@ -1,2 +1,42 @@
-public class Robot {
+public class Robot implements Move{
+    String name;
+
+    int maxHeight;
+    int maxRun;
+
+    boolean itCan;
+
+
+    public Robot(String name, int maxHeight, int maxRun) {
+        this.name = name;
+        this.maxHeight = maxHeight;
+        this.maxRun = maxRun;
+        this.itCan = true;
+    }
+
+    @Override
+    public void run(int dist) {
+        if (dist <= maxRun) {
+            System.out.println("Робот по имени " + name + " успешно пробежал " + dist + " метров");
+        } else {
+            System.out.println("Робот по имени " + name + " не справился с дистанцией " + dist + " метров");
+            itCan = false;
+        }
+
+    }
+
+    @Override
+    public void jump(int height) {
+        if (height <= maxHeight) {
+            System.out.println("Робот по имени " + name + " успешно прыгнул " + height + " метров");
+        } else {
+            System.out.println("Робот по имени " + name + " не справился с высотой " + height + " метров");
+        }
+
+    }
+
+    @Override
+    public boolean isItCan() {
+        return itCan;
+    }
 }
